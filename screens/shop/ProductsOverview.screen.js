74857cd00745a173cb6b26fe909753d63b6edcc0
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-11 07:33:32 +0700

    Setup store

---
+++ screens/shop/ProductsOverview.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const ProductsOverview = () => {
+  return <FlatList />;
+};
+
+export default ProductsOverview;
+
+const styles = StyleSheet.create({});
